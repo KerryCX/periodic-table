19e088281ff6a0e99e2885752a5d5577d69f7b30
Kerry Clements <kerryclements@me.com>
2026-09-25 12:54:32 +0100

    fix: separate tsconfig for tests to fix production build

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,9 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/setupTests.ts"],
+    typecheck: {
+      tsconfig: "./tsconfig.test.json",
+    },
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
